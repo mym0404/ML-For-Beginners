a6cbf7b94d11af5ca64a58be83057a5f05175c8b
MJ <mym0404@gmail.com>
2025-04-14 18:48:39 +0900

    chore: unconscious commit

---
+++ 3-Web-App/1-Web-App/web-app/app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('../ufo-model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+  return render_template('./index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+  int_features = [int(x) for x in request.form.values()]
+  final_features = [np.array(int_features)]
+  prediction = model.predict(final_features)
+
+  output = prediction[0]
+
+  countries = ['Austrailia', 'Canada', 'Germany', 'UI', 'US']
+
+  return render_template('index.html', prediction_text=f"Likely country: {countries[output]}")
+
+if __name__ == "__main__":
+  app.run(debug=True)
